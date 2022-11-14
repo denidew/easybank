f53d604aa9ff676931050b64b667372e1f2376fb
denibro <denisdewdew@gmail.com>
2022-11-14 16:14:35 +0700

    humbserger menu aktif

--- js.js
+++ js.js
@@ -2,15 +2,11 @@ const humbergermenu = document.querySelector('.hamburger-menu');
 const ham1 = document.querySelector('.ham1');
 const ham2 = document.querySelector('.ham2');
 const ham3 = document.querySelector('.ham3');
+const menu = document.querySelector('.menu')
 
 humbergermenu.addEventListener('click', () => {
     ham1.classList.toggle('tog');
-});
-
-humbergermenu.addEventListener('click', () => {
     ham2.classList.toggle('tog');
-});
-
-humbergermenu.addEventListener('click', () => {
     ham3.classList.toggle('tog');
+    menu.classList.toggle('toggle');
 });
