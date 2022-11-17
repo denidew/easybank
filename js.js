03fba5c75ac5bbe4b45cc028dba6f6bd1f2eb627
denibro <denisdewdew@gmail.com>
2022-11-17 17:33:05 +0700

    ok aja lah

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
-const humbergermenu = document.querySelector('.hamburger-menu');
+const humberger_menu = document.querySelector('.humberger-menu');
+const nav_link = document.querySelector('.nav-link');
 const ham1 = document.querySelector('.ham1');
 const ham2 = document.querySelector('.ham2');
 const ham3 = document.querySelector('.ham3');
 
-humbergermenu.addEventListener('click', () => {
-    ham1.classList.toggle('tog');
-    ham2.classList.toggle('tog');
-    ham3.classList.toggle('tog');
-});
+humberger_menu.addEventListener('click', () => {
+    nav_link.classList.toggle('tog')
+    ham1.classList.toggle('toghem')
+    ham2.classList.toggle('toghem')
+    ham3.classList.toggle('toghem')
+})
